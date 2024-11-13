120cf1fdb2069e0586d5da394df5885d2fad6cd6
Ayomide Oluniyi <ayorg360@gmail.com>
2024-11-13 20:32:12 +0200

    chore: add sticky scroll to header

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,8 +13,8 @@ export default function NotFoundPage() {
         <Image src='/logos/logo.svg' alt='The Grind Academy Logo' width={200} height={60} />
       </Link>
       <div className='relative mt-32 space-y-4 flex flex-col items-center'>
-        <p className='absolute left-1/2 -translate-x-1/2 -top-[70%] md:-top-[90%] font-gishaBold text-[160px] -z-10 text-[#1A2031] opacity-70 hover:text-primary hover:text-opacity-10 transition-all duration-700 select-none'>404</p>
-        <h1 className='font-gishaBold text-5xl text-center'>Oops! Page Not Found</h1>
+        <p className='absolute left-1/2 -translate-x-1/2 -top-[60%] md:-top-[90%] font-gishaBold text-[160px] -z-10 text-[#1A2031] opacity-70 hover:text-primary hover:text-opacity-10 transition-all duration-700 select-none'>404</p>
+        <h1 className='font-gishaBold text-4xl lg:text-5xl text-center'>Oops! Page Not Found</h1>
         <p className='text-muted-foreground text-center'>The page you&apos;re looking for doesn&apos;t exist or has been moved</p>
         <Button variant='default' href='/'>Return to Home Page</Button>
       </div>
