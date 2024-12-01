89976415b7c8b30fbe59a515e81fffd6fb5e8cff
Ayomide Oluniyi <ayorg360@gmail.com>
2024-12-01 19:28:15 +0200

    chore: change icons on course offerings

--- src/app/(main)/(home)/_components/LandingSection.tsx
+++ src/app/(main)/(home)/_components/LandingSection.tsx
@@ -9,7 +9,7 @@ export default function LandingSection() {
   const initialDelay = 300;
 
   return (
-    <div className='root-section !py-16 space-y-10'>
+    <div className='root-section !py-16 space-y-6'>
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 w-full'>
         <div className='space-y-6'>
           <div className='space-y-4'>
@@ -20,23 +20,23 @@ export default function LandingSection() {
               Where 90% Grind Meets 10% Talent.
             </h3>
           </div>
-          <div className="flex flex-wrap gap-4 justify-center lg:justify-start" data-aos='fade-right' data-aos-delay={initialDelay}>
-            <Button variant='outline' href='/courses'>Explore our courses</Button>
-            <Button href='/subscription'>Join the Grind Academy</Button>
-          </div>
-          <div className="flex items-center space-x-3 justify-center lg:justify-start" data-aos='fade-right' data-aos-delay={initialDelay}>
-            <p className='text-accent text-sm'>Over 1000+ students have enrolled and counting!</p>
-          </div>
         </div>
         <div className='hidden lg:flex flex-col justify-center relative' data-aos='fade-left'>
           <Blur className='absolute w-full h-40 translate-x-[44%]' />
           <BrandBars containerClassName='translate-x-[44%]' />
         </div>
       </div>
-      <div className='w-full border p-4' data-aos='fade-up' data-aos-delay={initialDelay}>
-        <video className='w-full h-[400px] lg:h-[680px] rounded-[2px] object-cover bg-primary-200/10 animate-pulse' poster='/images/default-thumbnail.jpg' autoPlay muted loop>
-          <source src='/videos/landing-video.mp4' type='video/mp4' />
+      <div className='w-full border p-4 bg-primary/5' data-aos='fade-up' data-aos-delay={initialDelay}>
+        <video className='w-auto mx-auto h-[400px] lg:h-[500px] rounded-[2px] object-cover bg-primary-200/10' poster='/images/default-thumbnail.jpg' controls autoPlay muted loop>
+          <source src='https://firebasestorage.googleapis.com/v0/b/suya-truck.appspot.com/o/Zahn%20Studios%2Fdammer-coffee.mp4?alt=media&token=437793d0-bd66-4544-aa6c-c7eee601cb02' type='video/mp4' />
         </video>
+      </div>
+      <div className="flex flex-wrap gap-4 justify-center !mt-10" data-aos='fade-up' data-aos-delay={initialDelay}>
+        <Button variant='outline' href='/courses'>Explore our courses</Button>
+        <Button href='/subscription'>Join the Grind Academy</Button>
+      </div>
+      <div className="flex items-center space-x-3 justify-center" data-aos='fade-up' data-aos-delay={initialDelay}>
+        <p className='text-accent text-sm'>Over 1000+ students have enrolled and counting!</p>
       </div>
     </div>
   )
