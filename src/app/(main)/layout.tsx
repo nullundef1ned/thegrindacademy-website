44fd69dfa2a4e46693c0a9733cfbe048f3fe12dc
Ayomide Oluniyi <ayorg360@gmail.com>
2025-01-21 01:40:29 +0200

    feat: add WhatsApp button to layout for improved user engagement
    - Introduced a new WhatsAppButton component to facilitate direct communication with users.
    - Integrated the WhatsAppButton into the main layout, enhancing accessibility for inquiries.
    - The button features a link to WhatsApp with a predefined message for user convenience.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer';
 import Preloader from './_components/Preloader';
+import WhatsAppButton from './_components/WhatsAppButton';
 
 type DefaultLayoutProps = {
   children: React.ReactNode;
@@ -10,6 +11,7 @@ type DefaultLayoutProps = {
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
   return (
     <main className='relative w-screen overflow-x-clip'>
+      <WhatsAppButton />
       <Header />
       <div className='relative min-h-screen space-y-4'>
         {children}
